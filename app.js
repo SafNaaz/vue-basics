fbfabf87974f9e48712047a547dc233c5c827b24
Safnas <iamsafnas@gmail.com>
2021-01-20 16:05:48 +0530

    toggling using v-if

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const app = Vue.createApp({
     data(){
         return{
+            showBooks: true,
             title : 'The Final Empire',
             author: 'Brandon Sanderson',
             age: 45
         }
     },
     methods: {
-        changeAge(){
-            this.age++
+        toggleShowBooks(){
+            this.showBooks = !this.showBooks
         }
     }
 })
